4a45efd0c01e63790f262e93050125e604373370
andersgoyt <88956565+andersgoyt@users.noreply.github.com>
2024-06-12 12:40:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ document.getElementById('download-btn').addEventListener('click', function() {
 
     // Redirect after a short delay to ensure download starts
     setTimeout(function() {
-        window.location.href = 'https://www.example.com'; // Update this to your redirect URL
+        window.location.href = 'https://andersgoyt.github.io/eni/'; // Update this to your redirect URL
     }, 1000); // Adjust the delay as needed
 });
